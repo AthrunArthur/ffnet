5419f0e6b7fb33bfb9b924af8469ead43e4a9878
sherryshare <sherryshare@126.com>
2012-09-28 11:38:29 +0800

    Add an infinete ping pong action.

--- example/ping_pong/client_main.cpp
+++ example/ping_pong/client_main.cpp
@@ -5,10 +5,23 @@
 #include <boost/serialization/array.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 
-void		onRecvPong(ffnet::PackagePtr_t pPong, ffnet::EndpointPtr_t pEP)
+void	sendPingMsg()
+{
+	char * pContent = new char[16];
+	const char *str = "ping world!";
+	std::memcpy(pContent, str, std::strlen(str) + 1); 
+	boost::shared_ptr<PingMsg> pMsg(new PingMsg(pContent, std::strlen(str) + 1));
+	ffnet::EndpointPtr_t tpp(new ffnet::Endpoint(ffnet::tcp_v4, boost::asio::ip::address_v4::from_string("127.0.0.1"), 8198));
+	ffnet::NetNervure::send(pMsg, tpp);
+	
+	std::cout<<"service running..."<<std::endl;
+}
+
+void	onRecvPong(ffnet::PackagePtr_t pPong, ffnet::EndpointPtr_t pEP)
 {
 	PongMsg & msg = *((PongMsg *)pPong.get());
 	std::cout<<"got pong!"<<std::endl;
+	sendPingMsg();
 }
 
 int main(int argc, char **argv) {
@@ -18,16 +31,9 @@ int main(int argc, char **argv) {
 	ffnet::NetNervureFromFile nnff("clnt_net_conf.ini");
 	
 	nnff.addNeedToRecvPkg<PongMsg>(onRecvPong);
+	sendPingMsg();
 	
 	
-	char * pContent = new char[16];
-	const char *str = "ping world!";
-	std::memcpy(pContent, str, std::strlen(str) + 1); 
-	boost::shared_ptr<PingMsg> pMsg(new PingMsg(pContent, std::strlen(str) + 1));
-	ffnet::EndpointPtr_t tpp(new ffnet::Endpoint(ffnet::tcp_v4, boost::asio::ip::address_v4::from_string("127.0.0.1"), 8198));
-	ffnet::NetNervure::send(pMsg, tpp);
-	
-	std::cout<<"service running..."<<std::endl;
 	nnff.run();
 	
 	
