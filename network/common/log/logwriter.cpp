5b6851406a15f17f2797dd9378d390f228a1ef49
AthrunArthur <athrunarthur@gmail.com>
2014-07-03 16:31:07 -0700

    fix log performance

--- network/common/log/logwriter.cpp
+++ network/common/log/logwriter.cpp
@@ -2,53 +2,47 @@
 
 namespace ffnet
 {
-	namespace details{
+namespace details {
 LogWriter::LogWriter()
-: m_strFilePath()
-, m_bRunning(true)
+    : m_strFilePath()
+    , m_bRunning(true)
 {
-	
+
 }
 void LogWriter::run(const char * filePath)
 {
-	m_strFilePath = String(filePath);
-	if(m_oIOThread)
-		return;
-	m_oIOThread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&LogWriter::actualRun, this)));
+    m_strFilePath = String(filePath);
+    if(m_oIOThread)
+        return;
+    m_oIOThread = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&LogWriter::actualRun, this)));
 }
 
 void LogWriter::actualRun()
 {
-	String str;
-	m_oFile.open(m_strFilePath.c_str() );
-	
-	m_oMutex.lock();
-	while(m_bRunning || !m_oQueue.empty())
-	{
-		m_oMutex.unlock();
-		size_t t = m_oQueue.size();
-		while(t!=0)
-		{
-			m_oQueue.pop(str);
-			m_oFile<<str<<std::endl;
-			t--;
-		}
-		m_oFile.flush();
-		m_oMutex.lock();
-	}
-	m_oMutex.unlock();
-	m_oFile.close();
+    String str;
+    m_oFile.open(m_strFilePath.c_str() );
+
+    while(m_bRunning || !m_oQueue.empty())
+    {
+        while(!m_oQueue.empty())
+        {
+            m_oQueue.pop(str);
+            m_oFile<<str<<std::endl;
+        }
+        m_oFile.flush();
+        m_oQueue.pop(str);
+        m_oFile<<str<<std::endl;
+    }
+    m_oFile.close();
 }
 
 LogWriter::~LogWriter()
 {
-	m_oMutex.lock();
-	m_bRunning = false;
-	m_oQueue.push_back("End and quit log!");
-	m_oMutex.unlock();
-	
-	m_oIOThread->join();
+    m_bRunning = false;
+    m_oQueue.push_back("End and quit log!");
+
+    m_oIOThread->join();
 }
 
-	}//end namespace details;
+}//end namespace details;
 }//end namespace ffnet
